70cfcedc51fceb705d1cde859013abab5ce586aa
Lee In Seok <dlstjr9068@naver.com>
2018-11-19 01:21:47 +0900

    이인석

--- 4주차/예제1.py
+++ 4주차/예제1.py
@@ -30,7 +30,14 @@ def coinChange(n) :
     n원을 돌려주기 위해 필요한 동전 개수의 최솟값을 반환하는 함수를 작성하세요.
     '''
 
-    return 0
+    coins = [100, 50, 10, 5, 1]
+    result = 0
+
+    for c in coins :
+        result += n // c
+        n -= c * (n//c)
+
+    return result
 
 def main():
     '''
--- 4주차/예제2.py
+++ 4주차/예제2.py
@@ -23,6 +23,13 @@
 
 import sys
 
+def getSlope(a, b) :
+    '''
+    점 a와 점 의 기울기의 절댓값을 구하는 함수
+    '''
+
+    return abs((b[1] - a[1]) / (b[0] - a[0]))
+
 def maxSlope(points) :
     '''
     n개의 점들 중에서 2개의 점을 선택했을 때, 얻을 수 있는 기울기의 절댓값 중에서 가장 큰 값을 반환하는 함수를 작성하세요.
@@ -31,7 +38,13 @@ def maxSlope(points) :
                기울기의 최댓값을 구하는 것에 집중해 주시길 바랍니다.
     '''
 
-    return 0
+    points.sort()
+    result = 0
+
+    for i in range(len(points)-1) :
+        result = max(result, getSlope(points[i], points[i+1]))
+
+    return result
 
 def main():
     '''
